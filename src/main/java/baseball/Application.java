c50605c629a3e0a8b1c8ded01326e6bd8c6ed75a
JunBe <wnsqowid0901@gmail.com>
2024-10-14 02:30:42 +0900

    초기 기능 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,95 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("test");
+        List<Integer> computer = getRandomNum(); //컴퓨터 수 임의지정
+        System.out.println(computer);
+        List<Integer> user = new ArrayList<>();
+        //여기 를 계속 반복해서 출력해야함
+        //기능 구현
+        while (true) {
+            String input = Console.readLine();
+            //잘못된 값 입력 시 예외 처리
+            exception(input);
+
+            for (int i = 0; i < 3; i++) {
+                user.add(input.charAt(i) - '0');
+            }
+//            System.out.println(user);
+            int strike = 0;
+            int ball = 0;
+            for (int i = 0; i < 3; i++) {
+                if (user.get(i) == computer.get(i)) {
+                    strike++;
+                } else if (user.contains(computer.get(i))) {
+                    ball++;
+                }
+            }
+            if (strike == 3) {
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int selectNextGame = Integer.parseInt(Console.readLine());
+                if (selectNextGame == 2) {
+                    return;
+                }// 종료후 게임 종료 or 재시작 로직 구현
+                computer = getRandomNum();
+            } else if (strike == 0 && ball == 0) {
+                System.out.println("낫싱");
+            } else if (strike > 0 && ball > 0) {
+                System.out.println(ball + "볼 " + strike + "스트라이크");
+            } else if (strike > 0) {
+                System.out.println(strike + "스트라이크");
+            } else if (ball > 0) {
+                System.out.println(ball + "볼");
+            }
+
+            for (int i = 0; i < 3; i++) {
+                user.remove(0);
+            }
+        }
+
     }
+
+    private static void exception(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3개의 값이 들어가야 합니다.");
+        }
+
+        try {
+            int parseInt = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수형 변환 불가");
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j < input.length(); j++) {
+                if (input.charAt(i) == input.charAt(j)) {
+                    throw new IllegalArgumentException("3개의 숫자는 모두 달라야 합니다.");
+                }
+            }
+        }
+
+    }
+
+    private static List<Integer> getRandomNum() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+
 }
